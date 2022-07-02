4244af7ed779b057366647d1777f084b7873c0dd
Grace Joh <graceeunaejoh@gmail.com>
2022-07-02 17:05:00 -0400

    mcr

--- app/controllers/perfumes_controller.rb
+++ app/controllers/perfumes_controller.rb
@@ -1,2 +1,38 @@
 class PerfumesController < ApplicationController
+  def index
+    perfumes = Perfume.all
+    render json: perfumes.as_json
+  end
+
+  def create
+    perfume = Perfume.new(
+      image: params[:image],
+      brand: params[:brand],
+      name: params[:name],
+      price: params[:price],
+    )
+    perfume.save
+    render json: perfume.as_json
+  end
+
+  def show
+    perfume = Perfume.find_by(id: params[:id])
+    render json: perfume.as_json
+  end
+
+  def update
+    perfume = Perfume.find_by(id: params[:id])
+    perfume.image = params[:image] || perfume.image
+    perfume.brand = params[:brand] || perfume.brand
+    perfume.name = params[:name] || perfume.name
+    perfume.price = params[:price] || perfume.price
+    perfume.save
+    render json: perfume.as_json
+  end
+
+  def destroy
+    perfume = Perfume.find_by(id: params[:id])
+    perfume.destroy
+    render json: { message: "Perfume successfully destroyed." }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/perfumes" => "perfumes#index"
+  post "/perfumes" => "perfumes#create"
+  get "/perfumes/:id" => "perfumes#show"
+  patch "/perfumes/:id" => "perfumes#update"
+  delete "/perfumes/:id" => "perfumes#destroy"
 end
