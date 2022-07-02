99da0988231c7b0d4422efa4ce2a8fd05deedb59
mattjkatz <matthewkatz7@gmail.com>
2022-07-02 16:09:30 -0500

    okay will do

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/collections" => "collections#index"
+  get "/collections/:id" => "collections#show"
+  post "/collections" => "collections#create"
+  patch "/collections/:id" => "collections#update"
+  delete "/collections/:id" => "collections#destroy"
 end
