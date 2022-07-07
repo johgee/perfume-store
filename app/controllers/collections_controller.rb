94ae173503a777eb0e50fdbad26782047ce35c7d
mattjkatz <matthewkatz7@gmail.com>
2022-07-07 16:52:31 -0500

    complete the collection crud methods and http requests

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -12,11 +12,24 @@ class CollectionsController < ApplicationController
 
   def create
     collection = Collection.new(
-      name: params[:name]
+      name: params[:name],
       description: params[:description]
     )
     collection.save
     render json: collection
+  end
+
+  def update
+    collection = Collection.find(params[:id])
+    collection.name = params[:name] || collection.name
+    collection.description = params[:description] || collection.description
+    render json: collection
+  end
+
+  def destroy
+    collection = Collection.find(params[:id])
+    collection.delete
+    render json: {message: "'#{collection.name}' successfully deleted!"}
   end
 
 end
