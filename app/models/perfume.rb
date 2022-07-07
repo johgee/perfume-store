e61f727cd0527e323d0c958bddc9910ac5acdaae
Grace Joh <graceeunaejoh@gmail.com>
2022-07-07 18:03:35 -0400

    export csv

--- app/models/perfume.rb
+++ app/models/perfume.rb
@@ -1,3 +1,15 @@
 class Perfume < ApplicationRecord
   acts_as_paranoid
+
+  def self.to_csv
+    attributes = %w{image brand name price}
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      all.find_each do perfume |
+        csv << attributes.map { |attr| user.send(attr) }       
+      end
+    end
+  end
 end
